a6545d307289c374589c4230aecec642e055fb0a
Kes <keskpeart@gmail.com>
2017-08-22 22:27:40 -0400

    Created test cases as well as made some minor changes to the contract.

--- migrations/1502995935_deploy_crowdsale.js
+++ migrations/1502995935_deploy_crowdsale.js
@@ -2,5 +2,5 @@ const Crowdsale = artifacts.require('Crowdsale')
 
 module.exports = function(deployer) {
   // Use deployer to state migration tasks.
-  deployer.deploy(Crowdsale)
+  deployer.deploy(Crowdsale, 0xa395650f5e23cb33fad82a0d1924747183b126a0, 737600, 737700)
 };
